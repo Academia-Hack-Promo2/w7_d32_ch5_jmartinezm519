ff68af837a8fcd27f833939874e9269e5ed14fa1
Jonathan Martinez <jamm@jmartinezm.org>
2015-04-19 12:47:37 -0430

    Todos los metodos operativos

--- transferencias.rb
+++ transferencias.rb
@@ -1,84 +1,161 @@
 require 'colorize'
-class Bank 
-  @@bank_name = "MyBank"
-  @@clients = []
 
-  def new_account(name, last_name, id_number, email, saving_amount=500, 
-                check_account=500)
-    @client = Client.new(name, last_name, id_number, email, saving_amount, 
-                check_account)
-
-    puts "La cuentas del cliente: "
-    puts "#{@client.name} #{@client.last_name}, CI: #{@client.id_number} "
-    puts "han sido creadas satisfactoriamente"
-    print "Numero de cuenta de ahorros:".colorize(:blue)
-    print "\t #{@client.saving_account}\n".colorize(:red)
-    print "Numero de cuenta de corriente:".colorize(:blue)
-    print"\t #{@client.check_account}\n".colorize(:red)
-    puts "Toda la informacion ha sido enviado al correo: #{@client.email}".colorize(:blue)
-   
-    @@clients << @client
-  end 
-
-  def balance
-  end
-
-  def self_transfer(origin_account, destination_account)
-
-    success_transfer
-  end
-
-  def others_transfer(origin_account, destination_account)
-
-    success_transfer
-  end
-
-  def clients
-    puts @@clients
-  end
-
-  private
-    def success_transfer
-      respuesta = "La tranferencia entre la cuenta #{origin_account}"\
-                  "y la cuenta #{destination_account} fue realizada"\
-                  "con exito"
-      puts respuesta
-    end
-
-end
-
-# Se instaciar nuevos clientes con 2 cuentas una de ahorro y otra corriente
+# Se instacia nuevos clientes con 2 cuentas una de ahorro y otra corriente
 # cada cuenta tiene un monto minimo de apertura por defecto de Bs.500
 # Las cuentas se generan automaticamente 
 
 class Client
-  attr_reader :name, :last_name, :id_number, :email, :saving_account, 
-              :saving_amount, :check_account, :check_amount
+  attr_accessor :name, :last_name, :id_number, :email, :saving_account, :saving_amount, :check_account, :check_amount
 
-  def initialize(name, last_name, id_number, email, saving_amount, 
-                check_account)
+  def initialize(name, last_name, id_number, email, saving_amount=500, check_amount =500)
     @name = name
     @last_name = last_name
     @id_number = "V-" + id_number.to_s
     @email = email
     @saving_account = "0105067230" + rand(0000000000..9999999999).to_s
-    @saving_amount = saving_account
     @check_account = "0105067240" + rand(0000000000..9999999999).to_s
+    @saving_amount = saving_amount
     @check_amount = check_amount
+  end
+
+  def to_s
+     format_client = "\n#{@id_number} #{@name} #{@last_name}"\
+                     "\n#{@email}"\
+                     "\n#{@saving_account} #{@saving_amount}"\
+                     "\n#{@check_account} #{@saving_amount}"
+    return format_client
   end
 
 end
 
+class Bank < Client
+
+  def initialize bank_name
+    @bank_name = bank_name
+  end
+
+  # Muestra el balance del saldo en las cuentas del cliente
+  def balance client
+      puts "Estimado Sr./Sra #{client.name} #{client.last_name}"
+      puts "Su saldo es:"
+      puts "Cuentas de ahorros: Bs. #{client.saving_amount}".colorize(:blue)
+      puts "Cuentas de corriente: Bs. #{client.check_amount}".colorize(:blue)
+  end
+
+  # Metodo para relizar transferencia entre las cuentas del cliente
+  def self_transfer(client, amount)
+    puts "Seleccione la cuenta origen de la cual trasladara los fondos (opciones ahorro o corriente):"
+    origin = gets.chomp.to_s.downcase
+    case origin
+    when "ahorro"
+      if client.saving_amount >= amount
+        client.saving_amount -= amount
+        client.check_amount += amount
+        success_transfer
+      else
+        no_money
+      end
+    when "corriente"
+      if client.check_amount >= amount
+        client.check_amount -= amount
+        client.saving_amount += amount
+        success_transfer
+      else
+        no_money
+      end
+    else 
+      return "Las opciones validas son ahorro y corriente"
+    end 
+  end
+
+  # Metodo para transferir a otros clientes del banco
+  def others_transfer_same_bank(client, client1, amount)
+    puts "Seleccione la cuenta origen de la cual trasladara los fondos (opciones ahorro o corriente):"
+    origin = gets.chomp.to_s.downcase
+    puts "Seleccione a que cuenta desea trasladar los fondos:"
+    puts "Ahorro #{client1.saving_account} opcion 1"
+    puts "Corriente #{client1.check_account} opcion 2"
+    destination = gets.chomp.to_i
+
+    if origin == "ahorro" && destination == 1
+      if client.saving_amount >= amount
+        client.saving_amount -= amount
+        client1.saving_amount += amount
+        success_transfer
+      else
+        no_money
+      end
+    elsif origin == "ahorro" && destination == 2
+      if client.check_amount >= amount
+        client.saving_amount -= amount
+        client1.check_amount += amount
+        success_transfer
+      else
+        no_money
+      end
+    elsif origin == "corriente" && destination == 1
+      if client.check_amount >= amount
+        client.check_amount -= amount
+        client1.saving_amount += amount
+        success_transfer
+      else
+        no_money
+      end
+    elsif origin == "corriente" && destination == 2
+      if client.saving_amount >= amount
+        client.check_amount -= amount
+        client1.check_amount += amount
+        success_transfer
+      else
+        no_money
+      end
+    else 
+      return "Las opciones validas son ahorro y corriente"
+    end 
+  end
+
+ # metodo para transferir a cuentas de otros bancos
+  def other_transfer_diferent_bank
+    puts "Seleccione la cuenta origen de la cual trasladara los fondos (opciones ahorro o corriente):"
+    origin = gets.chomp.to_s.downcase
+    puts "Escriba la cuenta a la que desea trasladar el dinero"
+    destination = gets.chomp.to_i
+    case origin
+    when "ahorro"
+      if client.saving_amount >= amount
+        client.saving_amount -= amount
+        success_transfer
+      else
+        no_money
+      end
+    when "corriente"
+      if client.check_amount >= amount
+        client.check_amount -= amount
+        success_transfer
+      else
+        no_money
+      end
+    else 
+      return "Las opciones validas son ahorro y corriente"
+    end 
+
+
+
+  end
+  private
+    def success_transfer
+      puts "La tranferenciafue realizada con exito".colorize(:blue)
+    end
+    def no_money
+      puts "El saldo en su cuenta origen es insuficiente".colorize(:red)
+    end
+
+end
 
 def main
-  my_bank = Bank.new
-  client_1 = my_bank.new_account("Jonathan", "Martinez", 18619519, 
-                                "jamm@jmartinezm.org")
-
-  client_2 = my_bank.new_account("Alberto", "Mercado", 17519619, 
-                                "jamm@outlook.com")
 
   
+
 end
 
 main
--- transferencias_bancarias.rb
+++ transferencias_bancarias.rb
@@ -24,42 +24,126 @@ require 'colorize'
 # de sus cuentas en cualquier momento (antes o después de las transferencias)
 
 class Bank
+  # def new
+  #   @client = Client.New(name, last_name, id_number, email)
+  # end
+
+  def transfer_self(account_from, account_to, amount)
+    
+
+  end
+
+  def transfer_other(account_from, account_to, amount)
+
+  end
 end
 
-class Client
+class Client < Bank
+  attr_accessor :name, :last_name, :id_number, :email, :saving_account, :saving_amount, :check_account, :check_amount
   def initialize(name, last_name, id_number, email)
     @name = name
     @last_name = last_name
-    @id_number = id_number
+    @id_number = "V-" + id_number.to_s
     @email = email
-    @saving_account = rand(20)
-    @check_account = rand(20)
+    @saving_account = "0105067230" + rand(0000000000..9999999999).to_s
+    @saving_amount = 100
+    @check_account = "0105067240" + rand(0000000000..9999999999).to_s
+    @check_amount = 100
   end
 
 end
 
 
-def hello
-  puts "\t\t\t========================".colorize(:blue)
-  puts "\t\t\tBienvenidos a MyBank web".colorize(:blue)
-  puts "\t\t\t========================".colorize(:blue)
-end
-
-def options
-  puts"\nSeleccione una opcion:\n"
-  puts "1- Crear cuenta en Mybank"
-  puts "2- Revisar saldo en cuenta"
-  puts "3- Realizar transferencia entre cuentas propias"
-  puts "4- Realizar transferencia a terceros en MyBank"
-  puts "5- Realizar transferencias a otros bancos"
-  puts "0- Salir del sistema".colorize(:red)
-  option = gets.chomp
-  return option
-end
-
 def main
-  hello
-  options
+
+  puts "\t\t\t========================".colorize(:blue)
+  puts "\t\t\tBienvenidos a MyBank web".colorize(:blue) 
+  puts "\t\t\t========================".colorize(:blue)
+
+  while true
+    puts"\nSeleccione una opcion:\n"
+    puts "1- Crear cuenta en Mybank"
+    puts "2- Revisar saldo en cuenta"
+    puts "3- Realizar transferencia entre cuentas propias"
+    puts "4- Realizar transferencia a terceros en MyBank"
+    puts "5- Realizar transferencias a otros bancos"
+    puts "0- Salir del sistema".colorize(:red)
+    print "Opcion: "
+    option = gets.chomp.to_s
+
+    if !["1","2","3","4","5","0"].include?(option)
+      puts "\nOpción invalida\n"
+      redo
+    end
+
+    case option
+      when "1"
+        puts
+        puts "Crearemos un nuevo usuario"
+        puts
+        puts "Introduzca su primer nombre"
+        name = gets.chomp
+        puts "Introduzca su primer apellido"
+        last_name =gets.chomp
+        puts "Introduzca su numero de cedula (solo numeros sin puntos)"
+        id_number = gets.chomp
+        puts "Introduzca su correo electronico"
+        email = gets.chomp
+        c1 = Client.new(name, last_name, id_number, email)
+        puts
+        puts "La cuentas del cliente #{c1.name} #{c1.last_name}, cedula: #{c1.id_number} han sido creadas satisfactoriamente".colorize(:blue)
+        puts "Numero de cuenta de ahorros: #{c1.saving_account}".colorize(:blue)
+        puts "Numero de cuenta de corriente: #{c1.check_account}".colorize(:blue)
+        puts "Toda la informacion ha sido enviado al correo: #{c1.email}".colorize(:blue)
+
+      when "2"
+        puts
+        if (defined?(c1))
+          puts "Su saldo es:"
+          puts "Cuentas de ahorros: Bs. #{c1.saving_amount}".colorize(:blue)
+          puts "Cuentas de corriente: Bs. #{c1.check_amount}".colorize(:blue)
+        else
+          puts
+          puts
+          puts
+          puts "=================================".colorize(:red)
+          puts "Por favor cree al cliente primero".colorize(:red)
+          puts "=================================".colorize(:red)
+          puts
+          puts
+          puts
+
+        end
+      when "3"
+        if (defined?(c1)).nil?
+          puts "Modulo de transferencias entre cuentas propias"
+
+        else
+          puts
+          puts
+          puts
+          puts "=================================".colorize(:red)
+          puts "Por favor cree al cliente primero".colorize(:red)
+          puts "=================================".colorize(:red)
+          puts
+          puts
+          puts
+        end
+      when "4"
+      when "5"
+      when "0"
+        puts
+        puts "\t\t\t==========================".colorize(:blue)
+        puts "\t\t\t\tHasta luego".colorize(:blue) 
+        puts "\t\t\t==========================".colorize(:blue)
+        abort
+
+
+
+        
+    end
+  end
+      
 end
 
 main
